aa3e17657dee5bd0b867e3ed56e2e10635aa8af0
HarryFoster47 <harryjfoster47@gmail.com>
2022-12-02 17:35:18 +0000

    first

---
+++ js/js.js
@@ -0,0 +1,56 @@
+// CRASH
+const button1 = document.querySelector("#bttn1");
+button1.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/ride.wav";
+	crash.play()
+})
+// Bass-drum
+const button2 = document.querySelector("#bttn2");
+button2.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/bass-drum.wav";
+	crash.play()
+})
+// Floor-tom
+const button3 = document.querySelector("#bttn3");
+button3.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/floor-tom.wav";
+	crash.play()
+})
+// Mid-tom
+const button4 = document.querySelector("#bttn4");
+button4.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/medium-tom.wav";
+	crash.play()
+})
+// Crash
+const button5 = document.querySelector("#bttn5");
+button5.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/crash.wav";
+	crash.play()
+})
+// High tom
+const button6 = document.querySelector("#bttn6");
+button6.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/high-tom.wav";
+	crash.play()
+})
+// Snare
+const button7 = document.querySelector("#bttn7");
+button7.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/snare.wav";
+	crash.play()
+})
+// High hat
+const button8 = document.querySelector("#bttn8");
+button8.addEventListener("click", () => {
+	const crash = new Audio();
+	crash.src = "assets/hi-hat.wav";
+	crash.play()
+})
